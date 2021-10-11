2c77e5949f5e0dbc7205fcbfaf1a96ffa56e9f8d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-11 18:10:36 -0500

    feat: render tasks dynamically

--- components/Task.vue
+++ components/Task.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="bg-work rounded-2xl relative flex justify-end">
+  <div
+    class="rounded-2xl relative flex justify-end h-40"
+    :class="`bg-${title.toLowerCase().replace(' ', '-')}`"
+  >
     <div class="overflow-hidden mr-5">
       <img
-        src="~/assets/icon-work.svg"
+        :src="
+          require(`~/assets/icon-${title.toLowerCase().replace(' ', '-')}.svg`)
+        "
         alt="Task icon"
         class="relative -top-2"
       />
     </div>
-    <div class="bg-dark-blue px-5 py-7 rounded-2xl absolute w-full top-12 space-y-1">
+    <div
+      class="
+        bg-dark-blue
+        px-5
+        py-7
+        rounded-2xl
+        absolute
+        w-full
+        top-12
+        space-y-1
+      "
+    >
       <div class="flex justify-between items-center">
-        <span class="font-medium text-white text-xl">Work</span>
-        <img src="~/assets/icon-ellipsis.svg" alt="Three dots" />
+        <span class="font-medium text-white text-xl">{{ title }}</span>
+        <svg
+          width="21"
+          height="5"
+          xmlns="http://www.w3.org/2000/svg"
+          class="
+            fill-current
+            text-desaturated-blue
+            hover:text-pale-blue
+            transition-colors
+          "
+        >
+          <path
+            d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+            fill-rule="evenodd"
+          />
+        </svg>
       </div>
       <div class="flex justify-between items-center">
         <span class="font-light text-white text-4xl">10hrs</span>
@@ -19,3 +50,16 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+// eslint-disable-next-line import/named
+import Vue, { PropType } from 'vue'
+import { Timeframes } from '~/app.t'
+
+export default Vue.extend({
+  props: {
+    title: { required: true, type: String },
+    timeframes: { required: true, type: Object as PropType<Timeframes> },
+  },
+})
+</script>
